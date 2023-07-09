738005ee6b13254a5c0197319d90aefcc9c7dc45
Nikhil <nikhildixit2125@gmail.com>
2023-07-09 23:34:32 +0530

    Added the OAuth Functionality- continue with google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,43 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { doc, serverTimestamp, getDoc, setDoc } from 'firebase/firestore'
 import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import { FcGoogle } from 'react-icons/fc'
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+import { useNavigate } from 'react-router'
 
 export default function OAuth() {
+    const navigate = useNavigate();
+
+    async function onGoogleClick() {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            // check for the user if already exist
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                })
+            }
+
+            navigate("/");
+
+        } catch (error) {
+            toast.error("Could not authorize with Google")
+        }
+    }
+
     return (
-        <button className='w-full flex items-center justify-center bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 rounded shadow-md hover:shadow-lg active:shadow-lg transition duration-200 ease-in-out'>
-            <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
+        <button type='button' onClick={onGoogleClick} className='w-full flex items-center justify-center bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 rounded shadow-md hover:shadow-lg active:shadow-lg transition duration-200 ease-in-out'>
+            <FcGoogle className='text-2xl bg-white rounded-full mr-2' />
             Continue with Google
         </button>
     )
